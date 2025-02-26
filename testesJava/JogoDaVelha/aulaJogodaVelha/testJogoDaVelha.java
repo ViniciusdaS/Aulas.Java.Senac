e21b651569379ee1f980c95e20fa12db1a569e81
Devonauta <oliveirateixeira653@gmail.com>
2025-02-26 17:01:48 -0300

    Add files via upload

---
+++ testesJava/JogoDaVelha/aulaJogodaVelha/testJogoDaVelha.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aulaJogodaVelha;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ *
+ * @author VINICIUSDASILVATEIXE
+ */
+
+
+
+public class testJogoDaVelha extends JogoDaVelha {
+    
+    /*@Test
+    public void verficarTabuleiro() {
+        
+    }*/
+    
+    @Test
+    public String[][] tabuleiro() {
+        if(tabuleiro != null) {
+            
+        }
+        return tabuleiro;
+    }
+    
+    @Test
+    public String testVez() {
+        if(vez.contains("X")) {
+            
+        }
+        return vez; 
+    }
+    @Test
+    public int testJogadas() {
+        if(jogadas == 0) {
+            
+        }
+        return jogadas; 
+    } 
+    
+    @Test
+    public int testButton() {
+        
+    }
+}
